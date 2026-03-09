961f8890ed12005606c8be75ad3bf6e046c5d177
Rahul-Devloper <rahulramesh26@yahoo.com>
2026-03-09 11:09:56 +0000

    minor changes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,8 @@
 export default function LoginPage() {
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <p className="text-gray-600">Login form coming in Session 2</p>
+      <h2 className='text-2xl font-bold mb-4'>Login</h2>
+      <p className='text-gray-600'>Ha Ha!!!Login form coming in Session 2</p>
     </div>
-  );
+  )
 }
